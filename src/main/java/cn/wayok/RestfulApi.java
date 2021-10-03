722ace1fabc1fbd85ef5a8b0d1672f2abbb60e71
icelox <2568450165@qq.com>
2021-10-03 19:54:17 +0800

    'add:main类'

---
+++ src/main/java/cn/wayok/RestfulApi.java
@@ -0,0 +1,13 @@
+package cn.wayok;
+
+/**
+ * @author icelo
+ * @date 2021/10/3
+ */
+public class RestfulApi {
+
+    public static void main(String[] args) {
+        System.out.println("Start here!");
+    }
+
+}
---
+++ src/test/java/cn/wayok/ApiTest.java
@@ -0,0 +1,9 @@
+package cn.wayok;
+
+/**
+ * @author icelo
+ * @date 2021/10/3
+ */
+public class ApiTest {
+
+}
