4112816f714ce3df1a795ffd40fc68d0a5477132
icelox <2568450165@qq.com>
2021-10-03 20:55:00 +0800

    '添加可以删除的TestXXX文件，为了git不能提交空文件夹的问题'

---
+++ src/main/java/cn/wayok/service/TestService.java
@@ -0,0 +1,9 @@
+package cn.wayok.service;
+
+/**
+ * @author icelo
+ * @date 2021/10/3
+ */
+public class TestService {
+
+}
---
+++ src/main/java/cn/wayok/utils/TestUtil.java
@@ -0,0 +1,9 @@
+package cn.wayok.utils;
+
+/**
+ * @author icelo
+ * @date 2021/10/3
+ */
+public class TestUtil {
+
+}
