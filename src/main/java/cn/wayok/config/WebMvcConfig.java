b938121e2b9ceed4bf92ca9f719dc71c46610517
icelox <2568450165@qq.com>
2021-10-16 00:34:01 +0800

    跨域问题

--- src/main/java/cn/wayok/config/WebMvcConfig.java
+++ src/main/java/cn/wayok/config/WebMvcConfig.java
@@ -1,7 +1,12 @@
 package cn.wayok.config;
 
 
+import javax.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +20,28 @@ public class WebMvcConfig implements WebMvcConfigurer {
         // Turn off suffix-based content negotiation
         configurer.favorPathExtension(false);
     }
+
+    @Bean
+    public Filter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("x-auth-token");
+        config.addExposedHeader("x-total-count");
+        config.setMaxAge(3600L);
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
 }
 
